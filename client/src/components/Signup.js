89f198bb29ed3b704ccd1a172151cc7acacb65e6
mdamian9 <m.damian9@hotmail.com>
2019-04-23 11:22:25 -0700

    Created signup form in landing page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -4,9 +4,31 @@ class Signup extends Component {
     render = () => {
         return (
             <div>
-                <button className="btn btn-secondary">
-                    Sign Up
-                </button>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>&ensp;
+                        <input className="form-control"
+                            placeholder="Enter your email..."
+                            name="email"
+                            type="email"
+                            id="email"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pwd">Password:</label>&ensp;
+                        <input className="form-control"
+                            placeholder="Create  your password..."
+                            name="password"
+                            type="password"
+                            id="pwd"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <button className="btn btn-secondary">
+                        Sign Up
+                    </button>
+                </form>
             </div>
         );
     };
