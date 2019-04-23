3da6b8fa6a0dc2d44b95ebbbcf602f8aac4e2e83
mdamian9 <m.damian9@hotmail.com>
2019-04-23 00:22:54 -0700

    Updated Login component

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -6,14 +6,14 @@ import '../assets/css/LandingPage.css';
 const LandingPage = () => {
     return (
         <div className="welcome d-flex align-items-center">
-            <div className="text-center" style={{ margin: '0 auto' }}>
-                <h1 style={{maxWidth: '75%', margin: '0 auto', fontSize: '2.5vw'}}>
+            <div style={{ margin: '0 auto' }}>
+                <h1 className="text-center" style={{maxWidth: '75%', margin: '0 auto', fontSize: '2.5vw'}}>
                     Welcome to cryptopapertrading.com, where you can practice trading cryptocurrencies without
                     risking real capital.
                 </h1>
                 <br />
-                <div className="d-flex justify-content-center">
-                    <Login /> &ensp; or &ensp; <Signup />
+                <div className="d-flex justify-content-center align-items-center">
+                    <Login /> &ensp;&ensp;&ensp; <h3><i>or</i></h3> &ensp;&ensp;&ensp; <Signup />
                 </div>
             </div>
         </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,9 +4,31 @@ class Login extends Component {
     render = () => {
         return (
             <div>
-                <button className="btn btn-primary">
-                    Log In
-                </button>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>&ensp;
+                        <input className="form-control"
+                            placeholder="Enter your email..."
+                            name="email"
+                            type="email"
+                            id="email"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pwd">Password:</label>&ensp;
+                        <input className="form-control"
+                            placeholder="Enter your password..."
+                            name="password"
+                            type="password"
+                            id="pwd"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <button className="btn btn-primary">
+                        Log In
+                    </button>
+                </form>
             </div>
         );
     };
