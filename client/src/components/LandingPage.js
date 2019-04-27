62d52726d841001d729876e40762d242483c821e
mdamian9 <m.damian9@hotmail.com>
2019-04-26 23:33:28 -0700

    Changed LandingPage to a class

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import { Button } from 'reactstrap';
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Login from './Login';
 import SignupForm from './SignupForm';
 import '../assets/css/LandingPage.css';
 
-const LandingPage = () => {
-    return (
-        <div>
-            <Login />
-            <div className="welcome d-flex align-items-center">
-                <div className="mx-auto">
-                    <h1 className="text-center" style={{ maxWidth: '75%', margin: '0 auto', fontSize: '2.5vw' }}>
-                        Welcome to cryptopapertrading.com, where you can practice trading cryptocurrencies without
-                        risking real capital.
-                    </h1>
-                    <br />
-                    <h3 className="text-center" style={{ maxWidth: '75%', margin: '0 auto', fontSize: '1.8vw' }}>
-                        Login or create a new account below!
-                    </h3>
-                    <br />
-                    <div className="d-flex justify-content-center align-items-center">
-                        <Button color="primary">
-                            Sign Up
-                        </Button>
+class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render = () => {
+        return (
+            <div>
+                <Login />
+                <div className="welcome d-flex align-items-center">
+                    <div className="mx-auto">
+                        <h1 className="text-center" style={{ maxWidth: '75%', margin: '0 auto', fontSize: '2.5vw' }}>
+                            Welcome to cryptopapertrading.com, where you can practice trading cryptocurrencies without
+                            risking real capital.
+                        </h1>
+                        <br />
+                        <h3 className="text-center" style={{ maxWidth: '75%', margin: '0 auto', fontSize: '1.8vw' }}>
+                            Login or create a new account below!
+                        </h3>
+                        <br />
+                        <div className="d-flex justify-content-center align-items-center">
+                            <Button color="primary">
+                                Sign Up
+                            </Button>
+                        </div>
                     </div>
-                    <SignupForm />
                 </div>
             </div>
-        </div>
-    );
+        );
+    };
 };
 
 export default LandingPage;
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-
 class SignupForm extends Component {
 
     handleChange = event => {
