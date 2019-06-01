9e5eb014b2e44e700daf1ce9bf41cb8fe271822e
mdamian9 <m.damian9@hotmail.com>
2019-05-31 23:54:13 -0700

    CalcRoiPage is functional

--- client/src/components/CalcRoiPage.js
+++ client/src/components/CalcRoiPage.js
@@ -7,9 +7,29 @@ class CalcRoiPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            roi_percent: '0.0',
-            roi_x: '0'
+            investment: '0.0',
+            divestment: '0.0',
+            roi_x: '0.0',
+            roi_percent: '0.0'
         };
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log(event.target.investment.value);
+        console.log(event.target.divestment.value);
+
+        const investment = event.target.investment.value;
+        const divestment = event.target.divestment.value;
+        const roi_x = divestment / investment;
+        const roi_percent = ((divestment / investment) - 1) * 100;
+
+        this.setState({
+            investment: event.target.investment.value,
+            divestment: event.target.divestment.value,
+            roi_x: roi_x.toFixed(2),
+            roi_percent: roi_percent.toFixed(2)
+        });
     };
 
     render = () => {
@@ -17,16 +37,16 @@ class CalcRoiPage extends Component {
             <div>
                 <NavbarComponent />
                 <br />
-                <Container>
+                <Container style={{ maxWidth: '60%' }}>
                     <Row>
                         <Col>
-                            <Form className="w-outline" style={{ padding: '15px' }}>
+                            <Form onSubmit={this.handleFormSubmit} className="mx-auto w-outline" style={{ padding: '15px' }}>
                                 <FormGroup>
-                                    <Label for="init-investment">Email</Label>
+                                    <Label for="init-investment">Initial investment:</Label>
                                     <Input type="text" name="investment" id="init-investment" placeholder="Enter initial investment" />
                                 </FormGroup>
                                 <FormGroup>
-                                    <Label for="final-divestment">Password</Label>
+                                    <Label for="final-divestment">Final divestment:</Label>
                                     <Input type="text" name="divestment" id="final-divestment" placeholder="Enter final divestment" />
                                 </FormGroup>
                                 <Button>Submit</Button>
@@ -35,13 +55,33 @@ class CalcRoiPage extends Component {
                     </Row>
                 </Container>
                 <br />
-                <Container>
-                    <Row className="text-center">
+                <Container className="text-center w-outline" style={{ maxWidth: '60%' }}>
+                    <Row>
+                        {/* Render this.state in these columns, test refresh when submit */}
                         <Col>
-                            Col 1
+                            <Row>
+                                <Col>
+                                    Initial investment: {this.state.investment}
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    Final divestment: {this.state.divestment}
+                                </Col>
+                            </Row>
                         </Col>
                         <Col>
-                            Col 2
+                            <Row>
+                                <Col>
+                                    ROI (multiple): {this.state.roi_x}x
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    ROI (percentage): {this.state.roi_percent}%
+                                </Col>
+                            </Row>
+
                         </Col>
                     </Row>
                 </Container>
