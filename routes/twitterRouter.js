7be5cfaebfe1cee3b10adda2ee4b385b4dbdb032
mdamian9 <m.damian9@hotmail.com>
2019-06-08 23:37:36 -0700

    Created twitterRouter, to implement Twitter API. Successfully connected and logged bitcoin tweets to console

--- routes/twitterRouter.js
+++ routes/twitterRouter.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Twit = require('twit');
+const apiKeys = require('../apiKeys');
 
 const Twitter = new Twit({
-    consumer_key: '...',
-    consumer_secret: '...',
-    access_token: '...',
-    access_token_secret: '...',
+    consumer_key: apiKeys.consumerKey,
+    consumer_secret: apiKeys.consumerSecret,
+    access_token: apiKeys.accessToken,
+    access_token_secret: apiKeys.accessTokenSecret,
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL: true,     // optional - requires SSL certificates to be valid.
 });
 
 router.get('/tweets/:hashtag', (req, res) => {
-    res.send(`Tweets for ${req.params.hashtag}`);
+    Twitter.get('search/tweets', { q: `${req.params.hashtag} since:2018-07-11`, count: 10 }, (err, data, res) => {
+        console.log(data.statuses);
+    });
+    res.send('Tweets');
 });
 
 router.post('/tweets/:hashtag', (req, res) => {
